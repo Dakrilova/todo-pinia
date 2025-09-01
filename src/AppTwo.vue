fb0ebf8254fd2fed4e7ea0c2af09ead7bcfc46d4
Dakrilova <dakrilova@gmail.com>
2025-09-01 15:46:30 +0300

    [add] todo

---
+++ src/AppTwo.vue
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia';
+
+export const useTodoStore = defineStore("todoStore", {
+    state: () => ({
+        item: String,
+    }),
+    getters:{
+        todo__list__complete(){
+            return this.state.filter((el) => el.isCompleted);
+        }
+    },
+    actions: {
+
+    }
+    
+})
